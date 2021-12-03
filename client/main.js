142f521ab6b7eb8b49d3828331bd12013cc81c64
Sofiane <makhouts@gmail.com>
2021-12-03 16:45:13 +0100

    must haves done, added extra styles

--- client/main.js
+++ client/main.js
@@ -2,19 +2,46 @@ let socket = io.connect();
 const buttonToAll = document.querySelector('.sendAll');
 const buttonSendToMe = document.querySelector('.sendToMe');
 const target = document.querySelector('.target');
+const guest = document.querySelector('.guests');
 
+const username = prompt('Welcome! Please choose an username:');
+socket.emit('sendUserName', (username));
 
 buttonToAll.addEventListener('click', () => {
     const message = document.querySelector('#message').value;
-    socket.emit('sendToAll', (message));
+    const messageObj = {
+       message: message,
+       username: username,
+       self: false     
+    }
+    if(message !== '') {
+        socket.emit('sendToAll', (messageObj));
+        message.textContent = '';
+    }
 })
 
 buttonSendToMe.addEventListener('click', () => {
     const message = document.querySelector('#message').value;
-    socket.emit('sendToMe', (message));
+    const messageObj = {
+        message: message,
+        username: username,
+        self: true 
+     }
+    if(message !== '') {
+        socket.emit('sendToMe', (messageObj));
+        message.textContent = '';
+    }
 })
 
-socket.on('displayMessage', (message) => {
-    target.innerHTML += '<br>'+message;
+socket.on('displayMessage', (messageObj) => {
+   const list = document.createElement('li');
+   list.textContent = messageObj.username + ': ' +messageObj.message;
+   if(messageObj.self) {
+       list.classList.add('self');
+   }
+   target.appendChild(list)
 });
 
+socket.on('displayUsername', (guests) => {
+    guest.textContent = guests;
+});
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const app = express();
 const clientPath = `${__dirname}/../client`;
 app.use(express.static(clientPath));
 const server = http.createServer(app);
+const guests = [];
 
 server.listen(8080, () =>{
     console.log("server running on "+8080);
@@ -14,13 +15,20 @@ server.listen(8080, () =>{
  const io = require('socket.io')(server);
  let counter = 0
 
+
+
  io.on('connection', (socket) => {
     counter++;
     console.log(counter+' someone connected');
-    socket.on('sendToAll', (message) =>{
-        io.emit("displayMessage", (message));
+    socket.on('sendToAll', (messageObj) =>{
+        io.emit("displayMessage", (messageObj));
     });
-});
+    socket.on('sendUserName', (username) => {
+        guests.push(username);
+        io.emit('displayUsername', (guests));
+        console.log(guests); 
+    })
+}); 
 
 io.on('connection', (socket) => {
     counter++;
@@ -29,4 +37,3 @@ io.on('connection', (socket) => {
         socket.emit("displayMessage", (message));
     });
 });
-
